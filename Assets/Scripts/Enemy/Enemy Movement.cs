0ccb56e8f56cbcee230e7b1a7bfdcc05f129dcdf
Ethan <Engerethan@gmail.com>
2021-04-09 11:32:33 -0600

    Working on enemy pt. 1

---
+++ Assets/Scripts/Enemy/Enemy Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Vector3 startPos;
+
+    //placeHolder playerTrigger
+    bool playerTrigger;
+    
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerTrigger == false)
+        {
+            EnemyMove();
+        }
+    }
+
+    void EnemyMove()
+    {
+
+    }
+}
