ac5ba149a2b9a26c60de585d6c6a6398ebfe47ee
Ethan <Engerethan@gmail.com>
2021-04-15 12:49:16 -0600

    Day 3
    Moved the enemy in a naturalish way finally

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -8,13 +8,12 @@ public class EnemyMovement : MonoBehaviour
     //change when you get the class for player
     Vector3 PlayerLocation = new Vector3(0, 0, 0);
 
-    Vector3 EnemyLocation = new Vector3();
+    Vector3 EnemyLocation = new Vector3(-2,1,-4);
 
-
-    //
+    
     public enum AIState { IDLE, PATROL, ATTACK};
 
-    AIState state = AIState.IDLE;
+    AIState state = AIState.PATROL;
 
     NavMeshAgent agent;
 
@@ -35,23 +34,28 @@ public class EnemyMovement : MonoBehaviour
         switch (state)
         {
             case AIState.IDLE:
-
+                //Animation of the zombie
+                Idle(Time.deltaTime);
                 break;
             case AIState.PATROL:
-                Patrol(Time.deltaTime);
+                Patrol(Time.deltaTime, EnemyLocation);
+                break;
+            case AIState.ATTACK:
+                Patrol(Time.deltaTime, PlayerLocation);
                 break;
         }
     }
-    void Idle()
+    void Idle(float delta)
     {
-        if(10 != 9)
+        //This needs to have come back to after I get the player class and i can pull the location
+        if (PlayerLocation == EnemyLocation);
         {
             state = AIState.ATTACK;
         }
     }
 
-    void Patrol(float delta)
+    void Patrol(float delta, Vector3 Destination)
     {
-
+        agent.Move(Destination*delta*speed);
     }
 }
